12be02d59635f0274c3216f0d881179d71663a59
yutingwu4 <scorp.only@gmail.com>
2021-05-08 13:11:48 -0700

    writing out outline html

---
+++ src/components/SmoothieCard.jsx
@@ -0,0 +1,23 @@
+/**
+ * @name SmoothieCard
+ * @desc Displays individual cards in SmoothieDisplay component.
+ */
+
+import React from "react";
+
+function SmoothieCard() {
+  return (
+    <div className="smoothieCard">
+      <div className="card-body">
+        <h5 className="card-title">
+          <span className="detailLabel">Smoothie Name</span>
+        </h5>
+        <p className="card-text">
+          <span className="detailLabel">Ingredients</span>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default SmoothieCard;
