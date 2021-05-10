de3bd5446d73a544052e67a4b5188ddf2ace3e01
yutingwu4 <scorp.only@gmail.com>
2021-05-10 13:14:00 -0700

    renamed files to js since no react

---
+++ src/components/assets/MockData.js
@@ -0,0 +1,26 @@
+// Dummy data:
+const cardHolder = [
+  {
+    name: "May Unicorn",
+    ingredients:
+      "Nonfat yogurt - 2 cups, Almond milk - 1 cup, Mixed berries - 1 cup, Sour gummibears, Sprinkles - 1 TB",
+    instructions: "Add cashews for texture, blend with ice.",
+    notes: "Calories: 200g, Sugar: 13g, Protein: 11g",
+  },
+  {
+    name: "Kevin",
+    ingredients:
+      "Nonfat greek yogurt - 2 cups, Almond milk - 2 cups, SUPER CREATINE",
+    instructions: "Eat with hot cheetos, blend with ice.",
+    notes: "Calories: 0, Sugar: 000000000",
+  },
+  {
+    name: "NoDramaLlama",
+    ingredients:
+      "Nonfat greek yogurt - 1/2 cup, Unsweetened almond milk - 1 cups, Frozen sliced bananas - 2 cups, Flax seeds - 1 TB, ground coffee beans - 1/4 cup, boba balls",
+    instructions: "Blend on high until smooth",
+    notes: "Calories: 300g, Sugar: 21g, Fiber: 4g, Protein 20g",
+  },
+];
+
+export default cardHolder;
