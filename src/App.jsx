96dcc3d30155096718254c156fedf9e13af659e9
yutingwu4 <scorp.only@gmail.com>
2021-05-10 13:09:41 -0700

    added logic for add, edit, delete functions - need to troubleshoot saveCardEdit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import SmoothieForm from "./components/SmoothieForm";
 import ViewSmoothies from "./components/ViewSmoothies";
 import NavBar from "./components/NavBar";
+import MockData from "./components/assets/MockData";
 
 /**
  * @terms
@@ -12,26 +13,72 @@ import NavBar from "./components/NavBar";
  */
 
 function App() {
+  //establish state for displaying all smoothie cards
+  const [smoothies, setSmoothies] = useState(MockData);
+
+  //to verify unique name
+  const isUniqueName = (name) => {
+    //iterate array of objects, verify name does not
+
+    //array.some
+    const isUnique = smoothies.some((smoothie) => smoothie.name !== name);
+
+    return isUnique;
+  };
+
+  //to add a smoothie card
+  const addCard = (newCard) => {
+    if (isUniqueName(newCard.name)) {
+      let temp = [...smoothies];
+      temp.push(newCard);
+      setSmoothies(temp);
+    } else {
+      alert("This name is already taken! Please enter a unique name.");
+    }
+  };
+
+  //to delete a smoothie card
+  const deleteCard = (card) => {
+    let temp = [...smoothies];
+    let indexToRemove = temp.indexOf(card);
+    temp.splice(indexToRemove, 1);
+    setSmoothies(temp);
+  };
+
+  //to edit a smoothie card; update state from global to local variable
+  const saveCardEdit = (card) => {
+    if (isUniqueName(card.name)) {
+      let temp = [...smoothies];
+      let indexToEdit = temp.indexOf(card);
+      temp[indexToEdit].name = card.name;
+      temp[indexToEdit].ingredients = card.ingredients;
+      temp[indexToEdit].instructions = card.instructions;
+      temp[indexToEdit].notes = card.notes;
+      console.log("temp", temp);
+      setSmoothies(temp);
+    } else {
+      alert("This name is already taken! Please enter a unique name.");
+    }
+  };
+
   return (
     <div className="parentContainer">
-      <NavBar />
       <Router>
-        <h1 className="slogan mt-5">Welcome to the New You</h1>
-        <form className="search-form">
-          <input
-            className="search-bar mt-5 ml-4"
-            type="text"
-            placeholder="Where's mah smoothie?"
-          />
-          <button type="button" className="btn btn-primary ml-4 mb-1">
-            Search
-          </button>
-        </form>
+        <NavBar />
 
         <div className="switchContainer">
           <Switch>
-            <Route exact path="/" component={ViewSmoothies} />
-            <Route path="/SmoothieForm" component={SmoothieForm} />
+            <Route exact path="/">
+              <ViewSmoothies
+                smoothies={smoothies}
+                deleteCard={deleteCard}
+                saveCardEdit={saveCardEdit}
+              />
+            </Route>
+
+            <Route path="/SmoothieForm">
+              <SmoothieForm addCard={addCard} />
+            </Route>
           </Switch>
         </div>
       </Router>
