078c60587d65f18d6de0dba34091d21ba883fe99
yutingwu4 <scorp.only@gmail.com>
2021-05-10 02:48:55 -0700

    write form for adding new smoothie

--- src/components/SmoothieForm.jsx
+++ src/components/SmoothieForm.jsx
@@ -9,6 +9,7 @@ function SmoothieForm() {
   const history = useHistory();
 
   //establish state for input fields
+  const [name, setName] = useState("");
   const [ingredients, setIngredients] = useState([]);
   const [instructions, setInstructions] = useState("");
   const [notes, setNotes] = useState("");
@@ -16,7 +17,79 @@ function SmoothieForm() {
   //onChange event handler to update state when key is pressed in input field
   // const addText = (e) => {};
 
-  return <div></div>;
+  return (
+    <div className="row justify-content-center mt-5">
+      <div className="col-6">
+        <form
+          // onSubmit={}
+          style={{ display: "flex", flexDirection: "column" }}
+        >
+          <label>
+            Name of Your Creation:
+            <input
+              className="form-control"
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+              type="text"
+              name="creation-name"
+              value={name}
+              required
+            />
+          </label>
+          <label>
+            Ingredients:
+            <textarea
+              className="form-control"
+              style={{ width: "100%" }}
+              onChange={(e) => {
+                setIngredients(e.target.value);
+              }}
+              type="text"
+              name="ingredients"
+              value={ingredients}
+              required
+            />
+          </label>
+          <label>
+            Instructions:
+            <textarea
+              className="form-control"
+              style={{ width: "100%" }}
+              onChange={(e) => {
+                setInstructions(e.target.value);
+              }}
+              type="text"
+              name="instructions"
+              value={instructions}
+              required
+            />
+          </label>
+          <label>
+            Notes:
+            <textarea
+              className="form-control"
+              style={{ width: "100%" }}
+              onChange={(e) => {
+                setNotes(e.target.value);
+              }}
+              type="text"
+              name="notes"
+              value={notes}
+              required
+            />
+          </label>
+
+          <button className="btn btn-primary" type="submit">
+            Add Smoothie
+          </button>
+          <button className="btn btn-secondary" type="submit">
+            Cancel
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default SmoothieForm;
