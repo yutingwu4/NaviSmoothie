34c20a90cf91291fde4ed3e0121a62de911eb455
yutingwu4 <scorp.only@gmail.com>
2021-05-09 18:03:45 -0700

    modularized NavBar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from "react";
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import SmoothieForm from "./components/SmoothieForm";
 import ViewSmoothies from "./components/ViewSmoothies";
+import NavBar from "./components/NavBar";
 
 /**
  * @terms
@@ -13,24 +14,9 @@ import ViewSmoothies from "./components/ViewSmoothies";
 function App() {
   return (
     <div className="parentContainer">
+      <NavBar />
       <Router>
-        <div className="navBar">
-          <li className="customLink title">Navi's Smoothies</li>
-          <ul className="navBarLinks">
-            <li className="list-inline-item">
-              <Link className="customLink" to="/">
-                View All
-              </Link>
-            </li>
-            <li className="list-inline-item">
-              <Link className="customLink" to="/SmoothieForm">
-                Create New
-              </Link>
-            </li>
-          </ul>
-        </div>
         <h1 className="slogan mt-5">Welcome to the New You</h1>
-
         <form className="search-form">
           <input
             className="search-bar mt-5 ml-4"
