5ccd5191f61bc83e6d9c0a8090f1f9ee6d18c531
yutingwu4 <scorp.only@gmail.com>
2021-05-10 20:50:43 -0700

    no edits

--- src/components/SmoothieDetail.jsx
+++ src/components/SmoothieDetail.jsx
@@ -3,21 +3,23 @@
  * @desc Component to show more details when user clicks each smoothie card.
  */
 
-import React from "react";
+ import React from "react";
 
-function SmoothieDetail({ ingredients, instructions, notes }) {
-  return (
-    <div className="smoothie-detail">
-      <h5 className="smoothie-detail-ing">Ingredients:</h5>
-      <p>{ingredients}</p>
-
-      <h5 className="smoothie-detail-inst">Instructions:</h5>
-      <p>{instructions}</p>
-
-      <h5 className="smoothie-detail-notes">Notes:</h5>
-      <p>{notes}</p>
-    </div>
-  );
-}
-
-export default SmoothieDetail;
+ function SmoothieDetail({ ingredients, instructions, notes }) {
+   //render close button inside return below, needs onClick prop -> invokes closePopUp
+   return (
+     <div className="smoothie-detail">
+       <h5 className="smoothie-detail-ing">Ingredients:</h5>
+       <p>{ingredients}</p>
+ 
+       <h5 className="smoothie-detail-inst">Instructions:</h5>
+       <p>{instructions}</p>
+ 
+       <h5 className="smoothie-detail-notes">Notes:</h5>
+       <p>{notes}</p>
+     </div>
+   );
+ }
+ 
+ 
+ export default SmoothieDetail;
