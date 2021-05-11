3d59b2a45142249148d9f4dd19c14088c333545f
yutingwu4 <scorp.only@gmail.com>
2021-05-11 02:53:43 -0700

    move index.js to root

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
-import "./index.scss";
+import App from "./src/App";
+import "./src/index.scss";
 
 ReactDOM.render(<App />, document.getElementById("root"));
